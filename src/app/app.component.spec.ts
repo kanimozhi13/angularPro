acf2640be8f7562eb9011e73f4fcf60e19b2ad00
kanimozhi13 <kanimozhi.mumca@gmail.com>
2021-01-12 18:37:45 +0000

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas:[CUSTOM_ELEMENTS_SCHEMA], //error occur
       imports: [
         RouterTestingModule
       ],
@@ -20,16 +22,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DisplayList'`, () => {
+  it(`should have as title 'Tour of Heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DisplayList');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DisplayList app is running!');
+    expect(compiled.querySelector('app-heroes')).not.toBe(null)
+   // expect(compiled.querySelector('h2').textContent).toContain('my heros');
   });
 });
